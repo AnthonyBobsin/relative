4e5f9bd85b14f8f033bd542458e6e28440ffecaa
AnthonyBobsin <bobsinj@gmail.com>
2014-12-29 16:18:51 +0800

    Fixed README.md

--- findWeather.php
+++ findWeather.php
@@ -1,7 +1,7 @@
 <?php
 
 	require_once 'forecast.io.php';
-	
+
 	date_default_timezone_set('America/Toronto');
 
 	$api_key = '94554c8a6559d0c2c5cd86c818780f32';
@@ -11,7 +11,6 @@
 	$yesterday = strtotime("-1 day", $current);
 	$forecast = new ForecastIO($api_key);
 	$data = array();
-	$
 	
 	
 	$Address = urlencode($_POST['location']);
@@ -23,17 +22,17 @@
 	    $Lat = $xml->result->geometry->location->lat;
 	    $Lon = $xml->result->geometry->location->lng;
 	    //Get current conditions and temperature
-	    $condition = $forecast->getCurrentConditions($Lat, $Lon, $units, $lang);
-	    $current_temp = (int)($condition->getTemperature());
+	    $current_condition = $forecast->getCurrentConditions($Lat, $Lon, $units, $lang);
+	    $current_temp = (int)($current_condition->getTemperature());
 		$data['current'] = $current_temp . "˚";
 		//Get conditions for yesterday
-		$condition = $forecast->getHistoricalConditions($Lat, $Lon, $units, $lang, $yesterday);
-		if ($condition == false) {
+		$yesterday_condition = $forecast->getHistoricalConditions($Lat, $Lon, $units, $lang, $yesterday);
+		if ($yesterday_condition == false) {
 			$data['yesterday'] = "Error";
 		}
 		else {
-			$yesterday_min = $condition->getMinTemperature();
-			$yesterday_max = $condition->getMaxTemperature();
+			$yesterday_min = $yesterday_condition->getMinTemperature();
+			$yesterday_max = $yesterday_condition->getMaxTemperature();
 			$yesterday_avg = (int)(($yesterday_max + $yesterday_min) / 2);
 			//Add: Check to see what time of day it is and return min/max/avg depending on that.
 			$data['yesterday'] = $yesterday_avg . "˚";
@@ -41,7 +40,7 @@
 
 	}
 	else {
-		$data['current'] = $data['yesterday'] = "Error Occured";
+		$data['current'] = $data['yesterday'] = "Format Error";
 	}
 
 	echo json_encode($data);
