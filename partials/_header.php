d65f9969c1d77a3bead3adc4097f739d5bfd7bff
AnthonyBobsin <bobsinj@gmail.com>
2015-01-17 16:13:28 -0500

    Remove .php from URL

--- partials/_header.php
+++ partials/_header.php
@@ -27,8 +27,8 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="./contact.php">Contact</a></li>
-        <li><a href="./about.php">About</a></li>
+        <li><a href="./contact">Contact</a></li>
+        <li><a href="./about">About</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
